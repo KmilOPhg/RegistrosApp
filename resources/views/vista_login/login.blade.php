2113aaa4d6595c31dfeef61baba74699ca9d2d36
Cookie <camilohurtado256@gmail.com>
2025-08-06 16:51:23 -0500

    Estilos al login

--- resources/views/vista_login/login.blade.php
+++ resources/views/vista_login/login.blade.php
@@ -1,36 +1,47 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="es">
 <head>
     <meta charset="UTF-8">
+    <title>Iniciar sesión</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Login</title>
+    @vite(['resources/css/login.css'])
+    <!-- FontAwesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Iniciar sesion</h1>
 
-    @if($errors->any())
-        <div class="alert alert-danger" id="mensaje-error">
-            <ul class="mb-0" >
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        <script>
-            let mensajeError = document.getElementById('mensaje-error');
-            setTimeout(() => {
-                mensajeError.style.display = 'none';
-            }, 2000);
-        </script>
-    @endif
-
-
-    <form method="POST" action="{{ route('login') }}">
+<form method="POST" action="{{ route('login') }}">
+    <h1>Iniciar sesión</h1>
+    <div class="inset">
         @csrf
-        <input type="email" name="email" id="txtEmail" placeholder="Email" required>
-        <input type="password" name="password" id="txtContra" placeholder="*********" required>
-        <button type="submit">Ingresar</button>
-    </form>
+
+        @if($errors->any())
+            <div class="alert">
+                @foreach($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+            </div>
+        @endif
+
+        <p>
+            <label for="email">Correo electrónico</label>
+            <input type="email" name="email" id="email" value="{{ old('email') }}" required>
+        </p>
+
+        <p>
+            <label for="password">Contraseña</label>
+            <input type="password" name="password" id="password" required>
+        </p>
+
+        <p>
+            <input type="checkbox" name="remember" id="remember">
+            <label for="remember">Recordarme</label>
+        </p>
+    </div>
+    <p class="p-container">
+        <input type="submit" value="Ingresar">
+    </p>
+</form>
+
 </body>
 </html>
